61cad8da3fc918ca4747482ea84a5cccf5a36f8c
PNShop <57126941+PNShop@users.noreply.github.com>
2019-10-29 13:50:42 +0700

    Update webhooks.php

--- Desktop/line-bot-unlimit_friend-master/webhooks.php
+++ Desktop/line-bot-unlimit_friend-master/webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = 'Channel_Access_Token';
+$access_token = 'sxHCl4N3RgD3iFO1YU2MQJ9UrT0jnaSWEcc/vXLXbR8qBvBkjemWVoYxN0raiVEpBq5uAHA+boVnpEvEHVuJjlKvk1oJEpfXMe66Z8fiZPpDIOQSqB9TXVNK8yWZL9/DXKGzc6bHCN5JZtm07j/bCwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -24,8 +24,8 @@ if (!is_null($events['events'])) {
 	}
 }
 require "vendor/autoload.php";
-$channelSecret = 'Channel_Secret';
-$pushID = 'Your_User_ID';
+$channelSecret = '1f26f9201d33655f65da19926e613e25';
+$pushID = 'U0bfcb10cf49ccbb73074507742bfdfe2';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text2);
